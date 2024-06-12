23cba10f994ea3f6723020eb8e524a2a247187f1
Martin Gerer <martingerer@gmail.com>
2024-06-12 23:36:58 +0200

    trying to fix the following error in swagger ui:
    Failed to fetch.
    Possible Reasons:
    
    CORS
    Network Failure
    URL scheme must be "http" or "https" for CORS request.

--- src/main/java/de/linksideal/shelfie/ShelfieApplication.java
+++ src/main/java/de/linksideal/shelfie/ShelfieApplication.java
@@ -1,9 +1,12 @@
 package de.linksideal.shelfie;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "any description of Server URL")})
 public class ShelfieApplication {
 
 	public static void main(String[] args) {
